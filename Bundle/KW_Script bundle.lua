dc21c335e76171995c71bb980635c110dc53cc2f
Carlos Wunsche <wunschecarlos@gmail.com>
2024-06-09 20:11:14 -0400

    update 1 file and create 1 file

--- Bundle/KW_Script bundle.lua
+++ Bundle/KW_Script bundle.lua
@@ -1,7 +1,8 @@
 -- @description KW Script Bundle
--- @version 1.11
+-- @version 1.12
 -- @changelog
---   + Added more scripts
+--  + Added a user configuration area
+--  * All scripts require this to access global variables
 -- @author Carlos Wunsche
 -- @metapackage
 -- @provides
@@ -12,3 +13,9 @@
 --    # Description
 --    * My personal script bundle.
 -- @link Github https://github.com/carloswunsche/
+
+------------------------
+-- User Configuration --
+------------------------
+
+THIS_IS_IT = 101
---
+++ Bundle/KW_Test.lua
@@ -0,0 +1,6 @@
+--  @noindex
+
+package.path = debug.getinfo(1,"S").source:match[[^@?(.*[\/])[^\/]-$]] .."?.lua;".. package.path
+require 'KW_Script bundle'
+
+reaper.ShowConsoleMsg(THIS_IS_IT)
