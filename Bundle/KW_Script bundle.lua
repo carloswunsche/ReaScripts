76e942ac3c8bf29c4fb1741f1e1388831fabc25f
Carlos Wunsche <wunschecarlos@gmail.com>
2024-09-11 11:39:38 -0400

    update 'KW_Script bundle.lua'

--- Bundle/KW_Script bundle.lua
+++ Bundle/KW_Script bundle.lua
@@ -1,10 +1,11 @@
 -- @description KW Script Bundle
--- @version 1.8
+-- @version 1.1
 -- @changelog
 --  + Added User Support
 -- @author Carlos Wunsche
 -- @metapackage
 -- @provides
+--  [nomain] .
 --  [nomain] KW_User.lua
 --  [main=main] KW_Reaper startup.lua
 --  [main=main] KW_Play-stop.lua
